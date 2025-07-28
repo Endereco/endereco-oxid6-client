66eb0ab57253dbef9efc2a67191b7e7ac3fa3e99
Heiko Frosch <Heiko.Frosch@emea.merkleinc.com>
2025-07-28 21:27:04 +0200

    Japanische Übersetzung

---
+++ application/translations/jp/enderecooxid6client_lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file contains translations.
+ *
+ * PHP Version 7
+ *
+ * @package   Endereco\OxidClient\Translations
+ * @author    Heiko Frosch
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License,
+ *            version 3 (GPLv3)
+ * @link      https://www.endereco.de/
+ */
+
+$sLangName  = "日本語";
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    'charset' => 'UTF-8',
+    'ENDERECOOXID6CLIENT_POPUP_HEADLINE' => '住所を確認してください',
+    'ENDERECOOXID6CLIENT_POPUP_SUBLINE' => '入力された住所は無効のようです。以下の提案から住所を選択してください。',
+    'ENDERECOOXID6CLIENT_POPUP_INPUT' => '入力された住所に問題があるようです。以下の選択肢から1つ選んでください。',
+    'ENDERECOOXID6CLIENT_POPUP_EDITINPUT' => '（編集）',
+    'ENDERECOOXID6CLIENT_POPUP_SUGGESTIONS' => '当社の提案：',
+    'ENDERECOOXID6CLIENT_POPUP_USE' => '選択された住所を使用する',
+    'ENDERECOOXID6CLIENT_POPUP_HEADLINE_GENERAL_ADDRESS' => '住所を確認してください',
+    'ENDERECOOXID6CLIENT_POPUP_HEADLINE_BILLING_ADDRESS' => '請求先住所を確認してください',
+    'ENDERECOOXID6CLIENT_POPUP_HEADLINE_SHIPPING_ADDRESS' => '配送先住所を確認してください',
+    'ENDERECOOXID6CLIENT_STATUS_EMAIL_NOT_CORRECT' => 'メールアドレスが正しくないようです',
+    'ENDERECOOXID6CLIENT_STATUS_EMAIL_CANT_RECEIVE' => 'メールボックスに到達できません',
+    'ENDERECOOXID6CLIENT_STATUS_EMAIL_SYNTAX_ERROR' => 'スペルを確認してください',
+    'ENDERECOOXID6CLIENT_STATUS_EMAIL_NO_MX' => 'メールアドレスが存在しません。スペルを確認してください。',
+
+    'ENDERECOOXID6CLIENT_mistakeNoPredictionSubline' => '住所を確認できません。入力内容を確認し、修正または確定してください。',
+    'ENDERECOOXID6CLIENT_notFoundSubline' => '住所を確認できません。入力内容を確認し、修正または確定してください。',
+    'ENDERECOOXID6CLIENT_confirmMyAddressCheckbox' => '私の住所が正しく配達可能であることを確認します。',
+    'ENDERECOOXID6CLIENT_confirmAddress' => '住所を確認する',
+    'ENDERECOOXID6CLIENT_editAddress' => '住所を編集する',
+    'ENDERECOOXID6CLIENT_warningText' => '誤った住所は配達の問題や追加費用を引き起こす可能性があります。',
+    'ENDERECOOXID6CLIENT_STATUS_buildingNumberIsMissing' => '番地が含まれていません。',
+    'ENDERECOOXID6CLIENT_STATUS_buildingNumberNotFound' => 'この番地は見つかりませんでした。',
+    'ENDERECOOXID6CLIENT_STATUS_streetNameNeedsCorrection' => '通り名のスペルが正しくありません。',
+    'ENDERECOOXID6CLIENT_STATUS_localityNeedsCorrection' => '市区町村名のスペルが正しくありません。',
+    'ENDERECOOXID6CLIENT_STATUS_postalCodeNeedsCorrection' => '郵便番号が無効です。',
+    'ENDERECOOXID6CLIENT_STATUS_countryCodeNeedsCorrection' => '入力された住所は他の国に存在します。',
+];
