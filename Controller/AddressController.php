f160460ed35535186e32cc7c191f672c25218b96
Florian Dressler <florian@endereco.de>
2026-02-05 11:32:19 +0100

    Integrate latest JS-SDK in Oxid 6 Checkout
    The JS-SDK was upgraded to the latest version to include recent bug
    fixes and improvements. Form handling in registration and checkout
    steps was adjusted to align with SDK changes, including initAms now
    expecting the whole selector as a parameter instead of a prefix. To
    simplify configuration and avoid confusion, the suffix option in
    integrator.js was removed. The remaining dependencies in package.json were
    updated to meet the new JS-SDK requirements. Additionally, the address
    flow was improved by implementing proper redirection when users edit
    the address and by handling AJAX-based updates when they accept
    corrections or keep the original address.
    
    For implementation details see #30.

--- Controller/AddressController.php
+++ Controller/AddressController.php
@@ -13,34 +13,39 @@ class AddressController extends \OxidEsales\Eshop\Application\Controller\Fronten
     public function render()
     {
         $data = json_decode(file_get_contents('php://input'), true);
+        $isChanged = 1;
         if ('editBillingAddress' == $data['method']) {
             // Save billing address.
             $oUser = oxNew(User::class);
             if ($oUser->load($data['params']['addressId'])) {
+
+                $hashBefore = $this->calculateHash(
+                    $oUser->oxuser__oxcountryid->rawValue, // Country ID
+                    $oUser->oxuser__oxzip->rawValue, // Postal code
+                    $oUser->oxuser__oxcity->rawValue, // Locality
+                    $oUser->oxuser__oxstreet->rawValue, // Street name
+                    $oUser->oxuser__oxstreetnr->rawValue, // House number
+                    $oUser->oxuser__oxaddinfo->rawValue // Additional info
+                );
+
+                $addressChanges = $this->getChangesFromPredictions($data['params']);
+
                 $oUser->oxuser__oxzip->rawValue
-                    = $data['params']['address']['postalCode']
-                        ? $data['params']['address']['postalCode']
-                        : $oUser->oxuser__oxzip->rawValue;
+                    = $addressChanges['postalCode'] ?? $oUser->oxuser__oxzip->rawValue;
 
                 $oUser->oxuser__oxcity->rawValue
-                    = $data['params']['address']['locality']
-                        ? $data['params']['address']['locality']
-                        : $oUser->oxuser__oxcity->rawValue;
+                    = $addressChanges['locality'] ?? $oUser->oxuser__oxcity->rawValue;
 
                 $oUser->oxuser__oxstreet->rawValue
-                    = $data['params']['address']['streetName']
-                        ? $data['params']['address']['streetName']
-                        : $oUser->oxuser__oxstreet->rawValue;
+                    = $addressChanges['streetName'] ?? $oUser->oxuser__oxstreet->rawValue;
 
                 $oUser->oxuser__oxstreetnr->rawValue
-                    = $data['params']['address']['buildingNumber']
+                    =$addressChanges['buildingNumber']
                         ? $data['params']['address']['buildingNumber']
                         : $oUser->oxuser__oxstreetnr->rawValue;
 
                 $oUser->oxuser__oxaddinfo->rawValue
-                    = $data['params']['address']['additionalInfo']
-                        ? $data['params']['address']['additionalInfo']
-                        : $oUser->oxuser__oxaddinfo->rawValue;
+                    = $addressChanges['additionalInfo'] ?? $oUser->oxuser__oxaddinfo->rawValue;
 
                 $status = implode(',', $data['params']['enderecometa']['status']);
                 $oUser->oxuser__mojoamsstatus->rawValue = $status;
@@ -57,6 +62,11 @@ class AddressController extends \OxidEsales\Eshop\Application\Controller\Fronten
                     $oUser->oxuser__oxstreetnr->rawValue, // House number
                     $oUser->oxuser__oxaddinfo->rawValue // Additional info
                 );
+
+                if($hash != $hashBefore){
+                    $isChanged = 2;
+                }
+
                 $oUser->oxuser__mojoaddresshash->rawValue = $hash;
                 $oUser->save();
             }
@@ -66,30 +76,32 @@ class AddressController extends \OxidEsales\Eshop\Application\Controller\Fronten
             // Save shipping address.
             $oAddress = oxNew(Address::class);
             if ($oAddress->load($data['params']['addressId'])) {
+
+                $hashBefore = $this->calculateHash(
+                    $oAddress->oxaddress__oxcountryid->rawValue, // Country ID
+                    $oAddress->oxaddress__oxzip->rawValue, // Postal code
+                    $oAddress->oxaddress__oxcity->rawValue, // Locality
+                    $oAddress->oxaddress__oxstreet->rawValue, // Street name
+                    $oAddress->oxaddress__oxstreetnr->rawValue, // House number
+                    $oAddress->oxaddress__oxaddinfo->rawValue // Additional info
+                );
+
+                $addressChanges = $this->getChangesFromPredictions($data['params']);
+
                 $oAddress->oxaddress__oxzip->rawValue
-                    = $data['params']['address']['postalCode']
-                        ? $data['params']['address']['postalCode']
-                        : $oAddress->oxaddress__oxzip->rawValue;
+                    = $addressChanges['postalCode'] ?? $oAddress->oxaddress__oxzip->rawValue;
 
                 $oAddress->oxaddress__oxcity->rawValue
-                    = $data['params']['address']['locality']
-                        ? $data['params']['address']['locality']
-                        : $oAddress->oxaddress__oxcity->rawValue;
+                    = $addressChanges['locality'] ?? $oAddress->oxaddress__oxcity->rawValue;
 
                 $oAddress->oxaddress__oxstreet->rawValue
-                    = $data['params']['address']['streetName']
-                        ? $data['params']['address']['streetName']
-                        : $oAddress->oxaddress__oxstreet->rawValue;
+                    = $addressChanges['streetName'] ?? $oAddress->oxaddress__oxstreet->rawValue;
 
                 $oAddress->oxaddress__oxstreetnr->rawValue
-                    = $data['params']['address']['buildingNumber']
-                        ? $data['params']['address']['buildingNumber']
-                        : $oAddress->oxaddress__oxstreetnr->rawValue;
+                    = $addressChanges['buildingNumber'] ?? $oAddress->oxaddress__oxstreetnr->rawValue;
 
                 $oAddress->oxaddress__oxaddinfo->rawValue
-                    = $data['params']['address']['additionalInfo']
-                        ? $data['params']['address']['additionalInfo']
-                        : $oAddress->oxaddress__oxaddinfo->rawValue;
+                    = $addressChanges['additionalInfo'] ?? $oAddress->oxaddress__oxaddinfo->rawValue;
 
                 $status = implode(',', $data['params']['enderecometa']['status']);
                 $oAddress->oxaddress__mojoamsstatus->rawValue = $status;
@@ -106,14 +118,18 @@ class AddressController extends \OxidEsales\Eshop\Application\Controller\Fronten
                     $oAddress->oxaddress__oxstreetnr->rawValue, // House number
                     $oAddress->oxaddress__oxaddinfo->rawValue // Additional info
                 );
+                if($hash != $hashBefore){
+                    $isChanged = 2;
+                }
                 $oAddress->oxaddress__mojoaddresshash->rawValue = $hash;
                 $oAddress->save();
             }
         }
 
-        \OxidEsales\Eshop\Core\Registry::getUtils()->showMessageAndExit('');
+        echo $isChanged;
+        \OxidEsales\Eshop\Core\Registry::getUtils()->showMessageAndExit($isChanged);
 
-        return '';
+        return $isChanged;
     }
 
     /**
@@ -145,5 +161,20 @@ class AddressController extends \OxidEsales\Eshop\Application\Controller\Fronten
             $additionalInfo
         ];
         return hash('sha256', implode('', $hashBody));
+    }
+
+    private function getChangesFromPredictions($data){
+
+        if($data){
+            if(isset($data['enderecometa']['predictions']) && count($data['enderecometa']['predictions']) > 0){
+                $predictions = $data['enderecometa']['predictions'];
+                foreach($predictions as $prediction){
+                    return $prediction;
+                }
+            }elseif($data['enderecometa']['status'][0] == "address_not_found"){
+                return $data['address'];
+            }
+
+        }
     }
 }
